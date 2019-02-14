e2026842cdb41fedd936df4bda6b59b3e3c57540
AJ Harmond <ajharmond@gmail.com>
2019-02-14 15:02:09 -0700

    Added answer reset easter egg and expanded answers.

--- eight_ball.rb
+++ eight_ball.rb
@@ -3,8 +3,17 @@
   "AS IT HAS BEEN SINCE THE BEGINNING OF THE UNIVERSE.",
   "...",
   "WHAT A CHILDISH THOUGHT...",
-  "IS THAT TRULY YOUR QUESTION? I SENSE SOMETHING ELSE PERTURBING YOU."
+  "IS THAT TRULY YOUR QUESTION? I SENSE SOMETHING ELSE PERTURBING YOU.",
+  "THAT IS INSIGNIFICANT. THE VOID CALL TO YOU.",
+  "PH'NGLUI MGLW'NAFH CTHULU R'LYEH WGAH'NAGL FHTAGN",
+  "YOG-SOTHOTH KNOWS THE GATE. YOG-SOTHOTH IS THE GATE. YOG-SOTHOTH IS THE KEY AND GUARDIAN OF THE GATE. PAST, PRESENT, FUTURE, ALL ARE ONE IN YOG-SOTHOTH. HE KNOWS WHERE THE OLD ONES BROKE THROUGH OF OLD, AND WHERE THEY SHALL BREAK THROUGH AGAIN.",
+  "THE OTHER GODS! THE OTHER GODS! THE GODS OF THE OUTER HELLS THAT GUARD THE FEEBLE GODS OF EARTH!... LOOK AWAY... GO BACK... DO NOT SEE! DO NOT SEE!",
+  "AND IF YOU GAZE LONG INTO AN ABYSS, THE ABYSS ALSO GAZES INTO YOU.",
+  "TO FIND EVERYTHING PROFOUND - THAT IS AN INCONVENIENT TRAIT.",
+  "AGAINST BOREDOM EVEN GODS STRUGGLE IN VAIN."
 ]
+
+@backupanswers = @answers.clone
 
 def wisdom
   puts "WHAT IS YOUR QUESTION?"
@@ -17,20 +26,11 @@ def wisdom
     puts "VERY WELL. BEGONE."
     exit
   when "add_answers"
-    puts "YOU BELIEVE YOUR WISDOM GREATER THAN MINE?"
-    puts "VERY WELL, WHAT WOULD YOU IMPART UPON ME?"
     add_answers
   when "print_answers"
-    puts "ONE SUCH AS YOU CANNOT CONTAIN MY WISDOM"
-    puts "..."
-    sleep (1)
-    puts "" 
-    puts "VERY WELL" 
-    puts ""
-    puts @answers
-    puts ""
-    sleep (2)
-    wisdom
+    print_answers
+  when "reset_answers"
+    reset_answers
   else
     if question.include?("?")
       puts @answers.sample
@@ -43,6 +43,8 @@ def wisdom
 end
 
 def add_answers
+  puts "YOU BELIEVE YOUR WISDOM GREATER THAN MINE?"
+  puts "VERY WELL, WHAT WOULD YOU IMPART UPON ME?"
   new_answer = gets.strip
   if @answers.include?(new_answer)
     puts "THAT IS KNOWN TO ME."
@@ -53,6 +55,27 @@ def add_answers
     puts ""
     wisdom
   end
+end
+
+def print_answers
+  puts "ONE SUCH AS YOU CANNOT CONTAIN MY WISDOM"
+  puts "..."
+  sleep (2)
+  puts "" 
+  puts "VERY WELL" 
+  puts ""
+  puts @answers
+  puts ""
+  sleep (2)
+  wisdom
+end
+
+def reset_answers
+  puts ""
+  puts "CLEARLY, YOU HAVE LEARNED THE ERROR OF YOUR WAYS."
+  @answers = @backupanswers
+  puts ""
+  wisdom
 end
 
 puts "WELCOME TRAVELLER TO THE MYSTERIOUS EIGHT BALL"
